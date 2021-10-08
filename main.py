7e3c1dbd500e39c8f265c0ff90e1d4d556fd4dbc
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-10-08 19:12:02 +0000

    Added the try and except function.

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 
 def collatz(number):
-    print("Collatz sequence for", user_input)
-    while number != 1:
-        if number % 2 == 1:
-            number = 3 * number + 1
-            print(number)
-        elif number % 2 == 0:
-            number = (number//2)
-            print(number)
-        else:
-            print("Enter a valid input.")
-        continue
-
-user_input = input("Please enter an integer: ")
-user_input = collatz(int(user_input))
+    if number % 2 == 0:
+        number = (number//2)
+        print(number)
+        return number
+    elif number == 0:
+        print("You cannot get the collatz of 0!")
+    else:
+        number = 3 * number + 1
+        print(number)
+        return number
 
 
-'''The output of this program could look something like this:
-
-Enter number:  (if 3 is what is entered...)
-3  
-10  
-5  
-16  
-8  
-4  
-2  
-1  
-'''
+print("Please enter an integer to get the collatz sequecne for it:  " )
+try:
+    user_input = int(input())
+    while user_input != 1:
+        user_input = collatz(user_input)
+except ValueError:  
+    print("Invalid argument! Please enter an integer.")
